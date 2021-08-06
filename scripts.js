b5fa42e0706b05dd670f39877e68563c4c7342bd
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-08-05 20:52:02 -0400

    Code not finished, up to part b/c

--- scripts.js
+++ scripts.js
@@ -2,8 +2,12 @@
 // Remember to pay attention to page loading!
 window.addEventListener("load", function () {
     const takeOffButton = document.getElementById("takeoff");
-    takeOffButton.addEventListener('click', function (){
+    takeOffButton.addEventListener('click', function () {
         const confirm1 = window.confirm('Are you positive we should take off')
-        
+        if (confirm1 === true) {
+            let fS1 = getElementbyID("flightStatus";
+            fS1.innerText("Shuttle in flight")
+        }
+
     })
 });

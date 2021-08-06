a1aae150f7351d5833b783868fe1381dfed80dba
Starr <starrleer@gmail.com>
2021-08-05 20:38:51 -0400

    confirm message in window

--- scripts.js
+++ scripts.js
@@ -2,4 +2,8 @@
 // Remember to pay attention to page loading!
 window.addEventListener("load", function () {
     const takeOffButton = document.getElementById("takeoff");
+    takeOffButton.addEventListener('click', function (){
+        const confirm1 = window.confirm('Are you positive we should take off')
+        
+    })
 });
